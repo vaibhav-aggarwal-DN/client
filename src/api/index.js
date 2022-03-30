5cc1f3c49e2469b331d47b9b4deb32a0b39a3a3d
vaibhav <vaibhav.aggarwal@doubtnut.com>
2022-03-31 02:36:52 +0530

    log env var

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const url = process.env.SERVER_URL || "http://localhost:3030/";
 
 export const fetchCases = async (city) => {
     try {
+        console.log("process.env.SERVER_URL ", process.env.SERVER_URL);
         let endpoint = 'total_cases';
         if (city) {
             endpoint = `city/${city}`
